4a8a20d0dd323f9d5df4cb670aa78165932d7e1b
vasilogi <jean.vasil@gmail.com>
2020-05-25 18:26:11 +0200

    create input_data.py
    a class for storing all the information concerning a certain data set

---
+++ classes/input_data.py
@@ -0,0 +1,44 @@
+# time
+# temperature
+# temperature units
+# conversion
+# the fitted conversion from the 'inverse fit'
+# the fitted time from the 'inverse fit'
+# the polynomial that describes the 'inverse fit' t = f(a)
+# the fitted conversion from the 'normal fit'
+# the fitted time from the 'normal fit'
+# the polynomial that describes the 'normal fit' a = f(t)
+# the reaction rate calculated according to the 'normal fit'
+
+class Input_Data:
+
+    """A class that stores the information for each different variable
+       that belongs to a certain data set
+       
+       mass        = experimentally recorded mass                             (array)
+       time        = experimentally recorded time                             (array)
+       temperature = experimental recorded temperature                        (array)
+       temp_units  = the units of the recorded temperature                    (string)
+       conversion  = experimentally recorded conversion                       (array)
+       a_invfit    = fitted conversion from the 'inverse fit'                 (array)
+       t_invfit    = fitted time from the 'inverse fit'                       (array)
+       t_poly      = the polynomial that describes the 'inverse fit' t = f(a) (class)
+       a_nrmfit    = fitted conversion from the 'normal fit'                  (array)
+       t_nrmfit    = fitted time from the 'normal fit'                        (array)
+       a_poly      = the polynomial that describes the 'normal fit' a = f(t)  (class)
+       rrate       = reaction rate calculated according to the 'normal fit'   (array)
+       """
+
+    def __init__(self, mass, time, temperature, temp_units, conversion, a_invfit, t_invfit, t_poly, a_nrmfit, t_nrmfit, a_poly, rrate):
+        self.mass        = mass
+        self.time        = time
+        self.temperature = temperature
+        self.temp_units  = temp_units
+        self.conversion  = conversion
+        self.a_invfit    = a_invfit
+        self.t_invfit    = t_invfit
+        self.t_poly      = t_poly
+        self.a_nrmfit    = a_nrmfit
+        self.t_nrmfit    = t_nrmfit
+        self.a_poly      = a_poly
+        self.rrate       = rrate
