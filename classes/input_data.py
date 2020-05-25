1e5cd170b431ef30d5450a8697ce68d55532a8a3
vasilogi <jean.vasil@gmail.com>
2020-05-25 19:00:04 +0200

    update input_data.py

--- classes/input_data.py
+++ classes/input_data.py
@@ -6,7 +6,7 @@ class Input_Data:
        mass        = experimentally recorded mass                             (array)
        time        = experimentally recorded time                             (array)
        temperature = experimental recorded temperature                        (array)
-       temp_units  = the units of the recorded temperature                    (string)
+       time_units  = the units of the recorded temperature                    (string)
        conversion  = experimentally recorded conversion                       (array)
        a_invfit    = fitted conversion from the 'inverse fit'                 (array)
        t_invfit    = fitted time from the 'inverse fit'                       (array)
@@ -17,11 +17,11 @@ class Input_Data:
        rrate       = reaction rate calculated according to the 'normal fit'   (array)
        """
 
-    def __init__(self, mass, time, temperature, temp_units, conversion, a_invfit, t_invfit, t_poly, a_nrmfit, t_nrmfit, a_poly, rrate):
+    def __init__(self, mass, time, time_units, temperature, conversion, a_invfit, t_invfit, t_poly, a_nrmfit, t_nrmfit, a_poly, rrate):
         self.mass        = mass
         self.time        = time
+        self.time_units  = time_units
         self.temperature = temperature
-        self.temp_units  = temp_units
         self.conversion  = conversion
         self.a_invfit    = a_invfit
         self.t_invfit    = t_invfit
