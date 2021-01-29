4904f4d396ddfe5209af71300ce943fc3afff2e3
vasilogi <jean.vasil@gmailcom>
2021-01-29 18:28:10 +0100

    separate conversion regressor from the calling scripts

--- conversionRegression.py
+++ conversionRegression.py
@@ -3,31 +3,11 @@
 # PACKAGES
 import os
 import numpy as np
-from scipy.optimize import curve_fit
 from classes.reaction_rates import Model
 from classes.arrhenius import rateConstant
+from classes.regressors import conversionRegression
 import pandas as pd
 import matplotlib.pyplot as plt
-
-def conversionRegression(time,conversion,model):
-    # perform Non-Linear Regression
-    # fit the experimental conversion (conversion)
-    # calculate the Arrhenius rate constant (k)
-
-    x          = time
-    y          = conversion
-    popt, pcov = curve_fit(model.alpha,x,y,p0=0.1)          # p0 : initial guess
-    # popt: optimal values for the parameters so that the sum of the squared residuals of f(xdata, *popt) - ydata is minimized.
-    k          = popt[0]                                    # Arrhenius rate constant
-    yfit       = np.array([model.alpha(t,k) for t in time]) # simulated conversion fraction
-
-    # calculate the determination coefficient
-    residuals = y - yfit
-    ss_res    = np.sum(residuals**2.0)
-    ss_tot    = np.sum((y-np.mean(y))**2.0)
-    r_squared = 1.0 - (ss_res / ss_tot)
-
-    return yfit, r_squared
 
 # DIRECTORIES
 MAIN_DIR  = os.getcwd()                   # current working directory
