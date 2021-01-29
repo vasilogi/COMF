4c4127d696412fc41feeb785575a224c0de7d2e8
vasilogi <jean.vasil@gmailcom>
2021-01-29 18:19:48 +0100

    working regression function

--- conversionRegression.py
+++ conversionRegression.py
@@ -9,6 +9,26 @@ from classes.arrhenius import rateConstant
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def conversionRegression(time,conversion,model):
+    # perform Non-Linear Regression
+    # fit the experimental conversion (conversion)
+    # calculate the Arrhenius rate constant (k)
+
+    x          = time
+    y          = conversion
+    popt, pcov = curve_fit(model.alpha,x,y,p0=0.1)          # p0 : initial guess
+    # popt: optimal values for the parameters so that the sum of the squared residuals of f(xdata, *popt) - ydata is minimized.
+    k          = popt[0]                                    # Arrhenius rate constant
+    yfit       = np.array([model.alpha(t,k) for t in time]) # simulated conversion fraction
+
+    # calculate the determination coefficient
+    residuals = y - yfit
+    ss_res    = np.sum(residuals**2.0)
+    ss_tot    = np.sum((y-np.mean(y))**2.0)
+    r_squared = 1.0 - (ss_res / ss_tot)
+
+    return yfit, r_squared
+
 # DIRECTORIES
 MAIN_DIR  = os.getcwd()                   # current working directory
 DATA      = os.path.join(MAIN_DIR,'data') # data directory
@@ -29,21 +49,7 @@ temperature  = df['temperature'].to_numpy()[0]
 # pick up the model
 model = Model('A2')
 
-# perform Non-Linear Regression
-# fit the experimental conversion (conversion)
-# calculate the Arrhenius rate constant (k)
+yfit, r_squared = conversionRegression(time, conversion, model)
 
-x          = time
-y          = conversion
-popt, pcov = curve_fit(model.alpha,x,y,p0=0.1)          # p0 : initial guess
-# popt: optimal values for the parameters so that the sum of the squared residuals of f(xdata, *popt) - ydata is minimized.
-k          = popt[0]                                    # Arrhenius rate constant
-yfit       = np.array([model.alpha(t,k) for t in time]) # simulated conversion fraction
-
-# calculate the determination coefficient
-residuals = y - yfit
-ss_res    = np.sum(residuals**2.0)
-ss_tot    = np.sum((y-np.mean(y))**2.0)
-r_squared = 1.0 - (ss_res / ss_tot)
-
-# def conversionRegression(time,conversion,model):
+plt.plot(time,conversion)
+plt.plot(time,yfit,linestyle='--')
